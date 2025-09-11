b9aed5c4e8259b1cc2ba2412015e22705cecb020
daniel <dany20082011@gmail.com>
2025-09-11 15:44:00 +0300

    feat: hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { Hero } from './components/Hero'
 
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <div className='max-w-7xl mx-auto px-6 pt-20'>
+        <Hero />
+      </div>
     </>
   )
 }
