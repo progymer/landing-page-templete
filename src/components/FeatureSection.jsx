66f980b805e0ab3d3473d11919d2702fca3e947c
daniel <dany20082011@gmail.com>
2025-09-13 20:54:57 +0300

    style: add a hover effect to the cards

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -16,7 +16,7 @@ export const FeatureSection = () => {
             <div className="flex flex-wrap mt-10 lg:mt-20">
                 {features.map((feature, index) => (
                     <div key={index} className="w-full sm:w-1/2 lg:w-1/3">
-                        <div className="flex">
+                        <div className="flex hover:scale-103 transition-all duration-300">
                             <div className="flex mx-6 h-10 w-10 p-2 bg-neutral-900 text-orange-700 justify-center items-center rounded-full">
                                 {feature.icon}
                             </div>
